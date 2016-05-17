ec4c41c29c63c2444533265abc1c088765722f42
Shahrooz <shahrooz84@gmail.com>
2016-05-16 17:55:37 -0700

    updated the gameboard, added random pop up function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("working")
+  function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bee2")) {
+        image.src = "images/bee123.gif";
+    } else {
+        image.src = "images/bee2.gif";
+    }
+}
+
+
+var myImg = document.querySelector('img')
+var newPosY = Math.random() * 600;
+var newPosX = Math.random() * 800;
+
+  myImg.style.top = newPosY + "px";
+  myImg.style.left = newPosX + "px";
+  // myImg.addEventListener("click");
