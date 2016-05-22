22f2884abbea163b6f52e90b7a546e0e4f8b2639
Shahrooz <shahrooz84@gmail.com>
2016-05-22 15:31:27 -0700

    .

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var game = {
     scoreBoard: document.querySelector("#scoreBoard2")
   },
   currentPlayer: null
-}
+};
 
 var beeSplat = $("#beeSplat")[0];
 var container = $("#container");
@@ -18,15 +18,15 @@ var $scoreBoard = $('#scoreBoard');
 var $scoreBoard2 = $('#scoreBoard2');
 var counter = 0;
 
-game.currentPlayer = game.player1
+game.currentPlayer = game.player1;
 
 function switchTurns() {
   if (game.currentPlayer == game.player1) {
     game.currentPlayer = game.player2;
   } else {
-    soundLoop.pause()
-    checkWinner()
-    clearAll()
+    soundLoop.pause();
+    checkWinner();
+    clearAll();
     temp = document.getElementById('countdown');
     temp.innerHTML = "GAME OVER!";
   }
@@ -34,14 +34,14 @@ function switchTurns() {
 
 var checkWinner = function(){
   if (game.player1.score > game.player2.score){
-    alert("Player 1 has won!")
+    alert("Player 1 has won!");
   } else if(game.player1.score == game.player2.score){
-      alert("It's a draw!")
+      alert("It's a draw!");
     }
     else {
-      alert("Player 2 has won!")
+      alert("Player 2 has won!");
     }
-  }
+  };
 
   $("#container").parent().css({position: 'relative'});
   $("#container").css({top: 100, left: 100, position:'absolute'});
@@ -53,50 +53,50 @@ var checkWinner = function(){
 
     container.append('<img class="bee" src="images/bee1234.gif">');
     this.$selector = $('img').last();
-    this.$selector.attr('style', 'top:' + this.y + 'px;left:' + this.x + 'px;')
+    this.$selector.attr('style', 'top:' + this.y + 'px;left:' + this.x + 'px;');
 
-    var self = this
+    var self = this;
     this.move = function(){
       this.$selector.animate({
         top: Math.round(Math.random() * 600),
         left: Math.round(Math.random() * 800)
       }, Math.round(Math.random() * 500) + 1000, function(){
-        self.move()
-      })
-    }
+        self.move();
+      });
+    };
 
-    this.move()
+    this.move();
 
     this.$selector.on('click', function(){
       beeSplat.currentTime = 0;
       beeSplat.play();
       if(game.currentPlayer == game.player1) {
         game.player1.score ++
-        new Bee(Date.now())
-        $scoreBoard.html(game.player1.score)
-        $(this).remove()
+        new Bee(Date.now());
+        $scoreBoard.html(game.player1.score);
+        $(this).remove();
       }
       else if (game.currentPlayer == game.player2) {
         game.player2.score ++
-        new Bee(Date.now())
-        $scoreBoard2.html(game.player2.score)
-        $(this).remove()
-        if (counter == 0) {
-          $('#countdown').html(15)
-          countdown()
-          counter ++
+        new Bee(Date.now());
+        $scoreBoard2.html(game.player2.score);
+        $(this).remove();
+        if (counter === 0) {
+          $('#countdown').html(15);
+          countdown();
+          counter ++ ;
         }
       }
-    })
+    });
   }
 
 function startGame(){
-  $("#start-game").off("click")
-  var bee1 = new Bee(Date.now())
-  var bee2 = new Bee(Date.now())
-  var bee3 = new Bee(Date.now())
-  var bee4 = new Bee(Date.now())
-  var bee5 = new Bee(Date.now())
+  $("#start-game").off("click");
+  var bee1 = new Bee(Date.now());
+  var bee2 = new Bee(Date.now());
+  var bee3 = new Bee(Date.now());
+  var bee4 = new Bee(Date.now());
+  var bee5 = new Bee(Date.now());
 
   soundLoop.play();
   countdown();
@@ -112,8 +112,8 @@ function startGame(){
     if (seconds == 1) {
       temp = document.getElementById('countdown');
       temp.innerHTML = "Next!";
-      alert("Time is up!")
-      switchTurns()
+      alert("Time is up!");
+      switchTurns();
       return;
     }
 
@@ -123,7 +123,7 @@ function startGame(){
     timeoutMyOswego = setTimeout(countdown, 1000);
   }
 
-  $("#start-game").on("click", startGame)
+  $("#start-game").on("click", startGame);
 
   $('#reset').click(function() {
     location.reload();
@@ -131,7 +131,7 @@ function startGame(){
 
 var clearAll = function() {
     clearAll = $('.bee').remove();
-  }
+  };
 
   soundLoop = new Audio('sounds/bee.mp3');
   if (typeof soundLoop.loop == 'boolean') {
